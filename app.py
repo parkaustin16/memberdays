f33c10662cbf7b74e1f6e2afbf84c67dca5a787c
parkaustin16 <parkaustin16@gmail.com>
2026-06-30 16:19:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -207,7 +207,7 @@ REGIONS = {
         ("fr", "France (FR)"),
         ("de", "Germany (DE)"),
         ("it", "Italy (IT)"),
-        ("br", "Belgium (BE)"),
+        ("be", "Belgium (BE)"),
         ("es", "Spain (ES)"),
         ("nl", "Netherlands (NL)"),
         ("cz", "Czech Republic (CZ)"),
